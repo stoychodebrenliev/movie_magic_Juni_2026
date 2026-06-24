f44106979c8c97f47e4ee777e96224da62997081
stoychodebrenliev <stoichodebrenliev@gmail.com>
2026-06-24 18:09:48 +0200

    add static middleware and homepage

--- src/index.js
+++ src/index.js
@@ -7,8 +7,12 @@ app.engine('hbs', engine());
 app.set('view engine', 'hbs');
 app.set('views', './src/views');
 
+app.use(express.static('./src/public'));
+
 app.get('/', (req, res) => {
-  res.render('home', {layout: false});
+  res.render('home', {layout: false});  
 });
+
+
 
 app.listen(5000, () => console.log('Server is listening on http://localhost:5000'));
