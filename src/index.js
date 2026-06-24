4d041e101d67d70ee65ac0141da2ed09f6a07f3a
stoychodebrenliev <stoichodebrenliev@gmail.com>
2026-06-24 17:56:58 +0200

    set up handlebars

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import express from 'express';
+import { engine } from 'express-handlebars';
 
 const app = express();
 
+app.engine('hbs', engine());
+app.set('view engine', 'hbs');
+app.set('views', './src/views');
+
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.render('home', {layout: false});
 });
 
 app.listen(5000, () => console.log('Server is listening on http://localhost:5000'));
