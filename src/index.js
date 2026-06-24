74abcb171bace795a05dbd4b2bbe49e245c8140c
stoychodebrenliev <stoichodebrenliev@gmail.com>
2026-06-24 17:32:56 +0200

    add express server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('Hello world');
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.listen(5000, () => console.log('Server is listening on http://localhost:5000'));
