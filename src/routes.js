cd067043a72988442392da71f6bf4f2be7646ded
stoychodebrenliev <stoichodebrenliev@gmail.com>
2026-06-26 15:35:34 +0200

    add 404 page

--- src/routes.js
+++ src/routes.js
@@ -9,4 +9,8 @@ const routes = Router();
 routes.use('/', homeController);
 routes.use('/movies', movieController);
 
+routes.get('*url', (req, res) => {
+    res.render('404')
+});  
+
 export default routes;
