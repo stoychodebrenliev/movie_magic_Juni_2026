5ef2f275053b707cba440fc3baae762bdce231d1
stoychodebrenliev <stoichodebrenliev@gmail.com>
2026-06-26 15:47:52 +0200

    add body parser

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -6,4 +6,12 @@ movieController.get('/create', (req, res) => {
     res.render('movies/create');
 });
 
+movieController.post('/create', (req, res) => {
+    console.log('post request received');
+
+    console.log(req.body);
+    
+    
+});
+
 export default movieController;
