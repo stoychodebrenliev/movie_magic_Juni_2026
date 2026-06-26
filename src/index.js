b104af7d05ce83ca226d27019bcbb50971ee8990
stoychodebrenliev <stoichodebrenliev@gmail.com>
2026-06-26 15:26:54 +0200

    add routes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import { engine } from 'express-handlebars';
-import homeController from './controllers/homeController.js';
-import movieController from './controllers/movieController.js';
+import routes from './routes.js';
+
 
 const app = express();
 
@@ -15,8 +15,7 @@ app.set('views', './src/views');
 app.use(express.static('./src/public'));
 
 //Setup routes
-app.use('/', homeController);
-app.use('/movies', movieController);
+app.use(routes);
 
 //Start the server
 app.listen(5000, () => console.log('Server is listening on http://localhost:5000'));
---
+++ src/routes.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+
+import homeController from "./controllers/homeController.js";
+import movieController from "./controllers/movieController.js";
+
+const routes = Router();
+
+
+routes.use('/', homeController);
+routes.use('/movies', movieController);
+
+export default routes;
