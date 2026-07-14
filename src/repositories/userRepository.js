4db227aa19ca337a4a96f72ae011372cf7f0b565
stoychodebrenliev <stoichodebrenliev@gmail.com>
2026-07-14 13:11:31 +0200

    create user plain password

--- src/repositories/userRepository.js
+++ src/repositories/userRepository.js
@@ -1,5 +1,14 @@
-export function create(userData) {
-    console.log('Creating user:', userData)
+import { prisma } from '../lib/prisma.js'
+
+export async function create(userData) {
+    const result = await prisma.user.create({
+        data: {
+            email: userData.email,
+            password: userData.password
+        }
+    });
+    
+    return result;
 }
 
 const userRepository = {
